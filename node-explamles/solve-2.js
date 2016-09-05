93b6eae9d7780699d6bd9091ae1ee3f99826421f
javega <javega@avantrip.com>
2016-09-05 18:10:56 -0300

    adding master

---
+++ node-explamles/solve-2.js
@@ -0,0 +1,20 @@
+var rect = require ('./rectangle-2.js');
+
+function solveRect (l,b){
+	console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+	rect(l,b,function(error,rectangle){
+		if(error){
+			console.log(error);
+		}else{
+			console.log("The area of a rectangle of dimensions length = "
+                 + l + " and breadth = " + b + " is " + rectangle.area());
+            console.log("The perimeter of a rectangle of dimensions length = "
+                 + l + " and breadth = " + b + " is " + rectangle.perimeter());
+		}
+	});
+};
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(-3,5);
